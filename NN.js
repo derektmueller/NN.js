677bab19d08c5c4f89499aec5020e99690307591
parenparen <cdaaar@gmail.com>
2015-02-01 15:34:23 -0800

    implemented fp and added logical test nets

--- NN.js
+++ NN.js
@@ -31,24 +31,46 @@ NN.prototype.J = function (Theta) {
  * Sigmoid activation
  */
 NN.prototype.g = function (X, Theta) {
-    console.log ('Theta = ');
-    console.log (Theta);
-    console.log ('X = ');
-    console.log (X);
-    return 1 / (1 + Math.pow (
-        Math.E, 
-        -math.multiply (
-            Theta, 
-            X
-        )));
+    if (X instanceof Array) {
+        return 1 / (1 + Math.pow (
+            Math.E, 
+            -math.multiply (
+                Theta, 
+                X
+            )));
+    } else {
+        return 1 / (1 + Math.pow (Math.E, -X));
+    }
+};
+
+/**
+ * Map helper which coerces argument to array
+ */
+NN.prototype.map = function (x, callback) {
+    x = x instanceof Array ? x : [x];
+    return math.map (x, callback); 
 };
 
 /**
  * @param Array X training example
  * @return Array hypothesis
  */
-NN.prototype.forwardProp = function (X) {
-
+NN.prototype.forwardProp = function (X, i) {
+    i = typeof i === 'undefined' ? this.L - 2 : i; 
+    if (i === -1) return X;
+    var that = this;
+//   console.log ('this.Theta[i]  = ');
+//   console.log (this.Theta[i] );
+//   console.log ('[1].concat (this.forwardProp (X, i - 1)) = ');
+//   console.log ([1].concat (this.forwardProp (X, i - 1)));
+    return this.map (
+        math.multiply (
+            this.Theta[i], 
+            // add the bias unit
+            [1].concat (this.forwardProp (X, i - 1))
+        ), function (elem) {
+            return that.g (elem);
+        });
 };
 
 NN.prototype.backProp = function () {
@@ -69,7 +91,7 @@ NN.prototype.initTheta = function (epsilon) {
     // count number of parameters
     var count = 0;
     for (var i = 0; i < this.S.length - 1; i++) {
-        count += this.S[i] * this.S[i + 1];
+        count += (this.S[i] + 1) * this.S[i + 1];
     }
 
     // randomly initialize parameters
@@ -91,11 +113,11 @@ NN.prototype.reshapeParams = function (Theta) {
         reshaped = [],
         Theta = math.clone (Theta);
     for (var i = 0; i < this.S.length - 1; i++) {
-        elementCount = this.S[i] * this.S[i + 1];
+        elementCount = (this.S[i] + 1) * this.S[i + 1];
         elements = Theta.slice (0, elementCount); 
         Theta = Theta.slice (elementCount);
         reshaped.push (
-            this.reshape (elements, [this.S[i + 1], this.S[i]]));
+            this.reshape (elements, [this.S[i + 1], this.S[i] + 1]));
     }
     return reshaped;
 };
@@ -129,13 +151,60 @@ return NN;
 if (typeof module !== 'undefined') module.exports = NN;
 
 GLOBAL.test = function () {
-    // test param initialization
-    var nn = new NN ([2, 5, 2]);
-    nn.initTheta ();
-    console.log ('nn.Theta = ');
-    console.log (nn.Theta);
 
-    // test activation
-    console.log (nn.g ([1, 1], nn.Theta[0][0]));
+    // basic functionality test
+    (function () {
+        // test param initialization
+        var nn = new NN ([2, 2, 1]);
+        nn.initTheta ();
+        console.log ('nn.Theta = ');
+        console.log (nn.Theta);
+        // test activation
+        console.log (nn.g ([1, 1, 1], nn.Theta[0][0]));
+    } ());
+
+    // AND network
+    (function () {
+        var nn = new NN ([2, 1]);
+        nn.Theta = [[-30, 20, 20]];
+        console.log (nn.forwardProp ([0, 0]));
+        console.log (nn.forwardProp ([0, 1]));
+        console.log (nn.forwardProp ([1, 0]));
+        console.log (nn.forwardProp ([1, 1]));
+    }) ();
+
+    // NAND network
+    (function () {
+        var nn = new NN ([2, 1]);
+        nn.Theta = [[10, -20, -20]];
+        console.log (nn.forwardProp ([0, 0]));
+        console.log (nn.forwardProp ([0, 1]));
+        console.log (nn.forwardProp ([1, 0]));
+        console.log (nn.forwardProp ([1, 1]));
+    }) ();
+
+    // OR network
+    (function () {
+        var nn = new NN ([2, 1]);
+        nn.Theta = [[-10, 20, 20]];
+        console.log (nn.forwardProp ([0, 0]));
+        console.log (nn.forwardProp ([0, 1]));
+        console.log (nn.forwardProp ([1, 0]));
+        console.log (nn.forwardProp ([1, 1]));
+    }) ();
+
+    // XNOR network
+    (function () {
+        // test param initialization
+        var nn = new NN ([2, 2, 1]);
+        nn.Theta = [
+            [[-30, 20, 20], [10, -20, -20]],
+            [[-10, 20, 20]]
+        ];
+        console.log (nn.forwardProp ([0, 0]));
+        console.log (nn.forwardProp ([0, 1]));
+        console.log (nn.forwardProp ([1, 0]));
+        console.log (nn.forwardProp ([1, 1]));
+    }) ();
 };
 
